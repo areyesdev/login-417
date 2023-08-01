8cf165c5c75fe7c6c9ed2f8565e7782fcc2a2d34
Andres Reyes <areyesdev@gmail.com>
2023-07-31 20:43:32 -0500

    Project login in react native

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { SafeAreaView, Text, View, TouchableOpacity, Image } from 'react-native'
 
-export default function App() {
+
+const LoginScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', backgroundColor: '#A7C1E2' }}>
+      <View style={{ alignItems: 'center' }}>
+        <Image style={{ width: 230, height: 79 }} source={require('./assets/logo.png')} />
+        <Image style={{ width: 340, height: 600 }} source={require('./assets/login.svg')} />
+      </View>
+      <TouchableOpacity onPress={() => { }} style={{ backgroundColor: '#6063FA', borderRadius: 10, padding: 20, margin: 30, marginTop: -5 }} >
+        <Text style={{ marginBotton: 30, color: '#fff', textAlign: 'center', fontSize: 18 }}>Login</Text>
+      </TouchableOpacity>
+      <Text style={{ marginBottom: 30, color: '#fff', textAlign: 'center', fontSize: 18 }}>No tienes una cuenta
+        <TouchableOpacity onPress={() => { }}>
+          <Text style={{ color: '#6063FA', fontWeight: 'bold', fontSize: 18 }}> Sign In</Text>
+        </TouchableOpacity>
+      </Text>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+export default LoginScreen
